ea99a43e46dcd6cc433199689406c122c7d10a53
i-618 <you@example.com>
2024-05-15 08:45:38 +0530

    Made code run in cli

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 import torchmetrics
 from langscript import Script, load_dataset_csv, get_dataloader
-from sequenceModel import Encoder, Decoder, train
+from sequenceModel import AttnDecoderRNN, Encoder, Decoder, train
 import torch
 import os
 
@@ -24,6 +24,7 @@ X_val, y_val = load_dataset_csv(f'{path}/{list_files[2]}')
 
 MAX_LENGTH = max([len(x) for x in X_train] + [len(y) for y in y_train])
 
+print('MAX_LENGTH', MAX_LENGTH)
 # Create the script object
 local_script = Script(language)
 local_script.create_char_vocab_from_words(y_train)
@@ -43,7 +44,9 @@ dataloader_val = get_dataloader(transliter_pairs_val, latin_script, local_script
 
 hidden_size = 128
 batch_size=32
-encoder = Encoder(input_size=latin_script.vocab_size, hidden_size=hidden_size, dropout_p=0).to(device)
-decoder = Decoder(hidden_size=hidden_size, output_size=local_script.vocab_size, device=device).to(device)
+encoder = Encoder(input_size=latin_script.vocab_size, hidden_size=hidden_size, num_layers=5,dropout_p=0).to(device)
+decoder = Decoder(hidden_size=hidden_size, max_length=MAX_LENGTH, output_size=local_script.vocab_size, num_decoder_layers=2,device=device).to(device)
 
-loss = train(dataloader_train, encoder, decoder, 10, print_every=1, plot_every=1, device=device)
+attn_decoder = AttnDecoderRNN(hidden_size=hidden_size, output_size=local_script.vocab_size, device=device).to(device)
+
+loss = train(dataloader_train, encoder, decoder, 25, print_every=1, plot_every=1, device=device, val_dataloader=dataloader_val)
--- sequenceModel.py
+++ sequenceModel.py
@@ -2,12 +2,12 @@ import torch
 import time, torchmetrics, math
 
 class Encoder(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, dropout_p=0.1):
+    def __init__(self, input_size, hidden_size, num_layers=2,dropout_p=0.1):
         super(Encoder, self).__init__()
         self.hidden_size = hidden_size
 
         self.embedding = torch.nn.Embedding(input_size, hidden_size)
-        self.gru = torch.nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.gru = torch.nn.GRU(hidden_size, hidden_size, num_layers=num_layers,batch_first=True)
         self.dropout = torch.nn.Dropout(dropout_p)
 
     def forward(self, input):
@@ -16,13 +16,14 @@ class Encoder(torch.nn.Module):
         return output, hidden
     
 class Decoder(torch.nn.Module):
-    def __init__(self, hidden_size, output_size, max_length=15, device=torch.device('cpu')):
+    def __init__(self, hidden_size, output_size, max_length=32, num_decoder_layers=3,device=torch.device('cpu')):
         super(Decoder, self).__init__()
         self.output_size = output_size
+        self.num_decoder_layers = num_decoder_layers
         self.max_length = max_length
         self.device = device
         self.embedding = torch.nn.Embedding(output_size, hidden_size)
-        self.gru = torch.nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.gru = torch.nn.GRU(hidden_size, hidden_size, num_layers=num_decoder_layers, batch_first=True)
         self.out = torch.nn.Linear(hidden_size, output_size)
 
     def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
@@ -47,12 +48,83 @@ class Decoder(torch.nn.Module):
         decoder_outputs = torch.nn.functional.log_softmax(decoder_outputs, dim=-1)
         return decoder_outputs, decoder_hidden, None # We return `None` for consistency in the training loop
 
-    def forward_step(self, input, hidden):
+    def forward_step(self, input:torch.Tensor, hidden:torch.Tensor):
         output = self.embedding(input)
         output = torch.nn.functional.relu(output)
-        output, hidden = self.gru(output, hidden)
+        if hidden.shape[0] != self.num_decoder_layers:
+            hidden = hidden.repeat(self.num_decoder_layers, 1, 1)
+        output, hidden = self.gru(output, hidden[-self.num_decoder_layers:])
         output = self.out(output)
         return output, hidden
+    
+class BahdanauAttention(torch.nn.Module):
+    def __init__(self, hidden_size):
+        super(BahdanauAttention, self).__init__()
+        self.Wa = torch.nn.Linear(hidden_size, hidden_size)
+        self.Ua = torch.nn.Linear(hidden_size, hidden_size)
+        self.Va = torch.nn.Linear(hidden_size, 1)
+
+    def forward(self, query, keys):
+        scores = self.Va(torch.tanh(self.Wa(query) + self.Ua(keys)))
+        scores = scores.squeeze(2).unsqueeze(1)
+
+        weights = torch.nn.functional.softmax(scores, dim=-1)
+        context = torch.bmm(weights, keys)
+
+        return context, weights
+
+class AttnDecoderRNN(torch.nn.Module):
+    def __init__(self, hidden_size, output_size, max_length=29, dropout_p=0.1, num_decoder_layers=3, device=torch.device('cpu')):
+        super(AttnDecoderRNN, self).__init__()
+        self.output_size = output_size
+        self.embedding = torch.nn.Embedding(output_size, hidden_size)
+        self.attention = BahdanauAttention(hidden_size)
+        self.gru = torch.nn.GRU(2 * hidden_size, hidden_size, num_layers=num_decoder_layers, batch_first=True)
+        self.out = torch.nn.Linear(hidden_size, output_size)
+        self.dropout = torch.nn.Dropout(dropout_p)
+        self.max_length = max_length
+        self.device = device
+
+    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=self.device).fill_(0)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+        attentions = []
+
+        for i in range(self.max_length):
+            decoder_output, decoder_hidden, attn_weights = self.forward_step(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_outputs.append(decoder_output)
+            attentions.append(attn_weights)
+
+            if target_tensor is not None:
+                # Teacher forcing: Feed the target as the next input
+                decoder_input = target_tensor[:, i].unsqueeze(1) # Teacher forcing
+            else:
+                # Without teacher forcing: use its own predictions as the next input
+                _, topi = decoder_output.topk(1)
+                decoder_input = topi.squeeze(-1).detach()  # detach from history as input
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_outputs = torch.nn.functional.log_softmax(decoder_outputs, dim=-1)
+        attentions = torch.cat(attentions, dim=1)
+
+        return decoder_outputs, decoder_hidden, attentions
+
+
+    def forward_step(self, input, hidden, encoder_outputs):
+        embedded =  self.dropout(self.embedding(input))
+
+        query = hidden.permute(1, 0, 2)
+        context, attn_weights = self.attention(query, encoder_outputs)
+        input_gru = torch.cat((embedded, context), dim=2)
+
+        output, hidden = self.gru(input_gru, hidden)
+        output = self.out(output)
+
+        return output, hidden, attn_weights
     
 
 def asMinutes(s):
@@ -78,9 +150,7 @@ def train_epoch(dataloader, encoder, decoder, encoder_optimizer,
         encoder_optimizer.zero_grad()
         decoder_optimizer.zero_grad()
 
-        encoder_outputs, encoder_hidden = encoder(input_tensor)
-        decoder_outputs, _, _ = decoder(encoder_outputs, encoder_hidden, target_tensor)
-
+        decoder_outputs = encode_decode_inp(encoder, decoder, input_tensor, target_tensor)
         loss = criterion(
             decoder_outputs.view(-1, decoder_outputs.size(-1)),
             target_tensor.view(-1)
@@ -96,6 +166,11 @@ def train_epoch(dataloader, encoder, decoder, encoder_optimizer,
         total_loss += loss.item()
 
     return total_loss / len(dataloader), total_accuracy
+
+def encode_decode_inp(encoder, decoder, input_tensor, target_tensor):
+    encoder_outputs, encoder_hidden = encoder(input_tensor)
+    decoder_outputs, _, _ = decoder(encoder_outputs, encoder_hidden, target_tensor)
+    return decoder_outputs
 
 def train(train_dataloader, encoder, decoder, n_epochs, learning_rate=0.001,
                print_every=100, plot_every=100, device=torch.device('cpu')
@@ -123,8 +198,9 @@ def train(train_dataloader, encoder, decoder, n_epochs, learning_rate=0.001,
             print_accuracy_avg = print_accuracy_total / print_every
             print_accuracy_total = 0
             print_loss_total = 0
-            print('%s (%d %d%%) Loss: %.4f Acc: %.2f %%' % (timeSince(start, epoch / n_epochs),
-                                        epoch, epoch / n_epochs * 100, print_loss_avg, print_accuracy_avg*100))
+            val_accuracy = calclulate_validation_accuracy(val_dataloader, accuracy_criterion, encoder, decoder, encode_decode_inp, device=device)
+            print(f'{timeSince(start, epoch / n_epochs)} ({epoch} {epoch / n_epochs * 100:.2f}%) Loss: {print_loss_avg:.4f} \
+                  Acc: {print_accuracy_avg*100:.2f} %, Val Acc: {val_accuracy*100:.2f} %')
 
         if epoch % plot_every == 0:
             plot_loss_avg = plot_loss_total / plot_every
@@ -134,3 +210,13 @@ def train(train_dataloader, encoder, decoder, n_epochs, learning_rate=0.001,
     
     return plot_losses
 
+def calclulate_validation_accuracy(val_dataloader, accuracy_criterion, encoder, decoder, encode_decode_inp, device=torch.device('cpu')):
+    total_accuracy = torch.tensor([], dtype=torch.float32, device=device)
+    for data in val_dataloader:
+        input_tensor, target_tensor = data
+        decoder_outputs = encode_decode_inp(encoder, decoder, input_tensor, target_tensor)
+        _, topi = decoder_outputs.topk(1)
+        decoded_ids = topi.squeeze()
+        accuracy = accuracy_criterion(decoded_ids, target_tensor)
+        total_accuracy = torch.cat((total_accuracy, accuracy))
+    return sum(total_accuracy ==1)/len(total_accuracy)
